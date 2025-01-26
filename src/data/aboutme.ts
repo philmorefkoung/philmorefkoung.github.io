28bc584ddaff439b0102487cae837ba82cd61e50
Philmore Koung <126188634+philmorefkoung@users.noreply.github.com>
2025-01-26 17:08:50 -0600

    Update aboutme.ts

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -18,22 +18,22 @@ export interface AboutMe {
 }
 
 export const aboutMe: AboutMe = {
-  name: "Jane R. Smith",
-  title: "Ph.D. Candidate",
-  institution: "Stanford University",
+  name: "Philmore Koung",
+  title: "Data Scientist",
+  institution: "University of Texas at Dallas",
   // Note that links work in the description
   description:
-    "I'm a final-year <a href='https://www.stanford.edu'>PhD candidate</a> working at the intersection of causal inference and machine learning. My research focuses on developing robust, interpretable systems that can reason about cause and effect in complex environments.",
-  email: "______@stanford.edu",
+    "I'm a final-year <a href='https://www.utdallas.edu/'>Master's student</a> working on improving deep learning algorithms for biomedical image analysis. My work focuses on augmenting deep learning models with topological data analysis to improve classification accuracy.",
+  email: "philmore.koung@gmail.com",
   imageUrl:
     "https://images.unsplash.com/photo-1581481615985-ba4775734a9b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  googleScholarUrl: "https://scholar.google.com/citations?user=bWtMl_MAAAAJ",
-  githubUsername: "janesmith",
-  linkedinUsername: "janesmith",
-  twitterUsername: "janesmith",
+  googleScholarUrl: "https://scholar.google.com/citations?user=H9kHnQsAAAAJ&hl=en",
+  githubUsername: "philmorefkoung",
+  linkedinUsername: "philmorekoung",
+  twitterUsername: "philmorekoung",
   blogUrl: "https://",
   cvUrl: "https://",
-  institutionUrl: "https://www.stanford.edu",
+  institutionUrl: "https://www.utdallas.edu/",
   // altName: "",
   // secretDescription: "I like dogs.",
 };
