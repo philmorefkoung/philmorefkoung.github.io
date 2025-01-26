0c1d4b5c673245fbfa3f18f697931654ae5a76ea
Philmore Koung <126188634+philmorefkoung@users.noreply.github.com>
2025-01-26 17:18:55 -0600

    Update news.ts

--- src/data/news.ts
+++ src/data/news.ts
@@ -7,10 +7,16 @@ export interface News {
 
 export const newsData: News[] = [
   // If you don't want to show news, just make the array empty.
-  /*{
-    date: "March 2024",
-    title: "Paper accepted at ICML 2024",
-    description: "Our work on causal discovery in time series data has been accepted at ICML 2024.",
-    link: "https://icml.cc/",
-  }*/
+*{
+    date: "October 2024",
+    title: "Paper accepted at IEEE 2024",
+    description: "Our work on few-shot learning in medical image analysis has been accepted at IEEE-EBMS International Conference on Biomedical and Health Informatics 2024.",
+    link: "https://bhi.embs.org/2024/",
+  }
+*{
+    date: "September 2024",
+    title: "Paper accepted at ML4H 2024",
+    description: "Our work on deep learning for biomedical image analysis has been accepted at ML4H 2024.",
+    link: "https://ahli.cc/ml4h/",
+  }
 ];
