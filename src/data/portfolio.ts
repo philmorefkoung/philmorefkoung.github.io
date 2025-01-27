94d28c9b7ea1635a8efe1dc332929f7703df48dc
Philmore Koung <126188634+philmorefkoung@users.noreply.github.com>
2025-01-27 17:14:11 -0600

    Update portfolio.ts

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -40,6 +40,16 @@ export const portfolioData: Portfolio[] = [
     codeUrl: "https://github.com/Kausta/topo-net",
   },
   {
+    title: "Retrieval Augmented Generation AI Agent for Resume and Job Matching",
+    description:
+      "Created an AI Agent using Meta's Llama 2 to match the best resumes to a given job description.",
+    technologies: ["Python", "Large Language Models", "RAG"],
+    projectUrl: "https://github.com/philmorefkoung/LLaMA2-chatbot-with-RAG/blob/main/Report.pdf",
+    imageUrl:
+      "https://images.unsplash.com/photo-1561622539-dffbfc2008fd?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    codeUrl: "https://github.com/philmorefkoung/LLaMA2-chatbot-with-RAG",
+  },
+  {
     title: "Luxury Watch Dataset",
     description:
       "A dataset containing over 280,000 real-world watch listings from chrono24, the world's largest online watch marketplace.",
