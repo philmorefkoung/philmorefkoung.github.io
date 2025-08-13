2afef49a880baf7a17a11b29f9ddd260be6b7407
Philmore Koung <126188634+philmorefkoung@users.noreply.github.com>
2025-08-13 18:34:52 -0500

    Update publication.ts

--- src/data/publication.ts
+++ src/data/publication.ts
@@ -33,7 +33,7 @@ export const publicationData: Publication[] = [
     codeUrl: "https://github.com/philmorefkoung/TopoDerma",
     tldr: "Combining Topological Deep Learning Embeddings with Vision Transformers to Improve Skin Cancer Detection",
     imageUrl:
-      "https://images.unsplash.com/photo-1663038509302-a20dc8a24ad8?q=80&w=1227&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1700929259310-10a0ba52de68?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     year: "2025",
@@ -44,7 +44,7 @@ export const publicationData: Publication[] = [
     codeUrl: "https://github.com/philmorefkoung/TopoVita-X",
     tldr: "Using topological deep learning to improve classification accuracy of cytomorphological images.",
     imageUrl:
-      "https://images.unsplash.com/photo-1663038509302-a20dc8a24ad8?q=80&w=1227&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1700929259310-10a0ba52de68?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     year: "2024",
